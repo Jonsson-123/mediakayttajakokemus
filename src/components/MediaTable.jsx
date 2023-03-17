ed646b4445bb32c4267c2ca9771ac082f5a5a45c
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-03-17 10:52:14 +0200

    edistystä

--- src/components/MediaTable.jsx
+++ src/components/MediaTable.jsx
@@ -1,14 +1,20 @@
 import React, {useEffect, useState} from 'react';
+import {baseUrl} from '../utils/variables';
 // import PropTypes from 'prop-types';
 import MediaRow from './MediaRow';
 
 const MediaTable = () => {
   const [mediaArray, setMediaArray] = useState([]);
   const getMedia = async () => {
-    const response = await fetch('test.json');
-    const json = await response.json();
-    setMediaArray(json);
-    // console.log(json);
+    const response = await fetch(baseUrl + 'media');
+    const files = await response.json();
+    const filesWithThumbnail = await Promise.all(
+      files.map(async (file) => {
+        const response = await fetch(baseUrl + 'media/' + file.file_id);
+        return await response.json();
+      })
+    );
+    setMediaArray(filesWithThumbnail);
   };
 
   useEffect(() => {
