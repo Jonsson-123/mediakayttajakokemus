a4dad5d165015a9ccabce33cafb2c0638dd45567
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-03-17 12:00:10 +0200

    week2ajax2  custom hooks done

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Viikko1 first app
 
-Open [https://users.metropolia.fi/~joonalam/mpjakk/week1/http-b](https://users.metropolia.fi/~joonalam/mpjakk/week1/http-b) to view it in the browser.
+Open [https://users.metropolia.fi/~joonalam/mpjakk/week2/ajax2](https://users.metropolia.fi/~joonalam/mpjakk/week2/ajax2) to view it in the browser.
--- src/components/MediaTable.jsx
+++ src/components/MediaTable.jsx
@@ -1,30 +1,9 @@
-import React, {useEffect, useState} from 'react';
-import {baseUrl} from '../utils/variables';
+import {useMedia} from '../hooks/apiHooks';
 // import PropTypes from 'prop-types';
 import MediaRow from './MediaRow';
 
 const MediaTable = () => {
-  const [mediaArray, setMediaArray] = useState([]);
-  const getMedia = async () => {
-    const response = await fetch(baseUrl + 'media');
-    const files = await response.json();
-    const filesWithThumbnail = await Promise.all(
-      files.map(async (file) => {
-        const response = await fetch(baseUrl + 'media/' + file.file_id);
-        return await response.json();
-      })
-    );
-    setMediaArray(filesWithThumbnail);
-  };
-
-  useEffect(() => {
-    try {
-      getMedia();
-    } catch (error) {
-      console.log(error.message);
-    }
-  }, []);
-  console.log(mediaArray);
+  const {mediaArray} = useMedia();
 
   return (
     <table>
