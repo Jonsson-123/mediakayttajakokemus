d0b6533d6b2478aeab7276ff59c9125d2d36da39
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-03-23 09:43:35 +0200

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import './App.css';
-import MediaTable from './components/Mediatable';
+import Home from './views/Home';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Layout from './views/Layout';
+import Single from './views/Single';
+import Profile from './views/Profile';
 
 const App = () => {
-  return <MediaTable />;
+  return (
+    <Router>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/single" element={<Single />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 };
 
 // add to App.jsx after imports
---
+++ src/views/Home.jsx
@@ -0,0 +1,12 @@
+import MediaTable from '../components/Mediatable';
+
+const Home = () => {
+  return (
+    <>
+      <h1>Home</h1>
+      <MediaTable />;
+    </>
+  );
+};
+
+export default Home;
