db564c52064c42d926170390064eb1e3588eb848
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-03-30 17:04:15 +0300

    registerii

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import useForm from '../hooks/FormHooks';
 import {useUser} from '../hooks/apiHooks';
+import {Button, TextField} from '@mui/material';
 
 const RegisterForm = (props) => {
   const {postUser, getCheckUser} = useUser();
@@ -37,34 +38,45 @@ const RegisterForm = (props) => {
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input
+        <TextField
+          fullWidth
+          margin="dense"
           name="username"
           placeholder="Username"
           onChange={handleInputChange}
           value={inputs.username}
           onBlur={handleUsername}
         />
-        <input
+        <TextField
+          fullWidth
+          margin="dense"
           name="password"
           type="password"
           placeholder="Password"
           onChange={handleInputChange}
           value={inputs.password}
         />
-        <input
+        <TextField
+          fullWidth
+          margin="dense"
           name="email"
           type="email"
           placeholder="Email"
           onChange={handleInputChange}
           value={inputs.email}
         />
-        <input
+        <TextField
+          fullWidth
+          margin="dense"
           name="full_name"
           placeholder="Full name"
           onChange={handleInputChange}
           value={inputs.full_name}
         />
-        <button type="submit"> Register</button>
+        <Button fullWidth sx={{mt: 1}} type="submit" variant="contained">
+          {' '}
+          Register
+        </Button>
       </form>
     </>
   );
