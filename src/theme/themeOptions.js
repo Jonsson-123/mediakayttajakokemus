604ed32f99642baea1a14c7a435d30028da9e434
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-03-30 12:04:00 +0300

    30.3 tunti

---
+++ src/theme/themeOptions.js
@@ -0,0 +1,12 @@
+import '@fontsource/anonymous-pro';
+import '@fontsource/alegreya';
+
+const themeOptions = {
+  typography: {
+    fontFamily: 'Anonymous Pro',
+  },
+  body1: {
+    fontFamily: 'alegreya',
+  },
+};
+export {themeOptions};
