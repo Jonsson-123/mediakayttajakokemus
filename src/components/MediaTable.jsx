6eeb0d07c83f952be6fd6f45d09c587c47740b35
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-04-06 10:26:51 +0300

    kopsattu Ilen ulkoasu homeen

--- src/components/MediaTable.jsx
+++ src/components/MediaTable.jsx
@@ -1,13 +1,15 @@
 import {ImageList} from '@mui/material';
+import PropTypes from 'prop-types';
 import {useMedia} from '../hooks/apiHooks';
-// import PropTypes from 'prop-types';
+import {useWindowSize} from '../hooks/WindowHooks';
 import MediaRow from './MediaRow';
 
 const MediaTable = () => {
   const {mediaArray} = useMedia();
+  const windowSize = useWindowSize();
 
   return (
-    <ImageList>
+    <ImageList cols={windowSize.width > 768 ? 3 : 2} gap={8}>
       {mediaArray.map((item, index) => {
         return <MediaRow key={index} file={item} />;
       })}
