73bf7741cd96c8b3ded3655c6dfd2c92288113ff
Jonsson-123 <98164794+Jonsson-123@users.noreply.github.com>
2023-04-06 09:43:52 +0300

    Update Upload.jsx

--- src/views/Upload.jsx
+++ src/views/Upload.jsx
@@ -1,14 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Box, Button} from '@mui/material';
+import useForm from '../hooks/FormHooks';
 
 const Upload = (props) => {
+  const initValues = {
+    title: '',
+    description: '',
+    file: '',
+  };
+
+  const doUpload = () => {};
+  const {inputs, handleSubmit, handleInputChange} = useForm(
+    doUpload,
+    initValues
+  );
+
+  console.log('upload', inputs);
+
   return (
     <Box>
       <form>
-        <input type="text" name="title" value="title" />
-        <textarea name="textarea"></textarea>
-        <input type="file" name="file" accept="image/* video/* audio/*" />
+        <input
+          onChange={handleInputChange}
+          type="text"
+          name="title"
+          value={inputs.title}
+        />
+        <textarea
+          onChange={handleInputChange}
+          name="description"
+          value={inputs.description}
+        ></textarea>
+        <input
+          onChange={handleInputChange}
+          type="file"
+          name="file"
+          accept="image/* video/* audio/*"
+        />
         <Button type="submit">Upload</Button>
       </form>
     </Box>
